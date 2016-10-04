1a37972822f568ccd496920093802340e7f1d243
gruand <gruand@gmail.com>
2016-10-04 16:32:33 +0300

    deleted unused import

--- src/main/java/com/grudiy/namehandler/Application.java
+++ src/main/java/com/grudiy/namehandler/Application.java
@@ -2,7 +2,6 @@ package com.grudiy.namehandler;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
